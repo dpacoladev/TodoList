c2be1532037edaed5af48045bcb388a20914b4fa
dpacoladev <dpacola.dev@gmail.com>
2025-03-08 11:35:26 -0300

    Classe da Entidade Todo

---
+++ src/main/java/dpacoladev/com/github/TodoList/entity/Todo.java
@@ -0,0 +1,64 @@
+package dpacoladev.com.github.TodoList.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Boolean realizado;
+    private Integer prioridade;
+
+    public Todo() {}
+
+    public Todo(String nome, String descricao, Boolean realizado, Integer prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(Boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public Integer getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Integer prioridade) {
+        this.prioridade = prioridade;
+    }
+}
