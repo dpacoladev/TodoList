de854486ab757278b2eb6f83999cbd47f2a6c1ce
dpacoladev <dpacola.dev@gmail.com>
2025-03-08 11:28:13 -0300

    First Commit

---
+++ src/test/java/dpacoladev/com/github/TodoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package dpacoladev.com.github.TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
